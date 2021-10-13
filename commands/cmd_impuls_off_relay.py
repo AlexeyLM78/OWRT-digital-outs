abfa4c52187e9a2357a0dac875ca1af216fa52c3
Aleksandr Shenshin <aashenshin@yandex.ru>
2021-10-13 23:40:44 +0300

    Epic7.V1.S1 Task:31286959 Add CLI command impuls_off_relay

---
+++ commands/cmd_impuls_off_relay.py
@@ -0,0 +1,22 @@
+import click
+import sys
+
+@click.command()
+@click.option('-r', '--relay', default='', help='Unique identifier of the relay')
+@click.option('-t', '--time', default='', help='Pulse time in seconds')
+def main(relay, time):
+    '''Set relay to OFF position on X seconds, after switches to ON state'''
+    try:
+        import ubus
+    except ImportError:
+        print('Failed import ubus.')
+        sys.exit(-1)
+
+    if not ubus.connect("/var/run/ubus.sock"):
+        print("Failed connect to ubus")
+        sys.exit(-1)
+
+    print(ubus.call("owrt_digital_outs", "impuls_off_relay", {"id_relay":relay,"time":time}))
+
+if __name__ == "__main__":
+    main()
